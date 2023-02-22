6e3086ca935b9fc74c54d0747fc12edd76a36785
Kyle White <kylewhite1019@gmail.com>
2023-02-22 13:09:20 -0500

    Adding Movement

--- My project/Assets/GameScript.cs
+++ My project/Assets/GameScript.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    public float walkingSpeed = .5f;
+    public float runningSpeed = 10f;
+    public Rigidbody rb;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    public void Movement()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            print("Forward");
+            rb.velocity = transform.forward * walkingSpeed;
+        }
+
+        else if (Input.GetKey(KeyCode.G))
+        {
+            print("Backwards");
+            rb.velocity = -transform.forward * walkingSpeed;
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            print("Left");
+            rb.velocity = -transform.right * walkingSpeed;
+            
+        }
+        else if (Input.GetKey(KeyCode.H))
+        {
+            print("Right");
+            rb.velocity = transform.right * walkingSpeed;
+        }
     }
 }
